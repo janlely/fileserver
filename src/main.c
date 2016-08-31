2dc2acc802900ed7b752e46f7093b6db8b474d15
Junjie Jin <janlely@163.com>
2016-08-31 11:11:12 +0800

    mod main.c

--- src/main.c
+++ src/main.c
@@ -17,53 +17,6 @@ void* httpserver_Dispatch(void *arg);
 void httpserver_GenericHandler(struct evhttp_request *req, void *arg);
 void httpserver_ProcessRequest(struct evhttp_request *req);
 
-/* void generic_handler(struct evhttp_request *req, void *arg) */
-/* { */
-    /* struct evbuffer *buf = evbuffer_new(); */
-    /* [> struct evbuffer *input_buf = evhttp_request_get_input_buffer(req); <] */
-    /* if(!buf) */
-    /* { */
-        /* puts("failed to create response buffer \n"); */
-        /* return; */
-    /* } */
-
-    /* handler_request(req, buf); */
-
-    /* [> evbuffer_add_printf(buf, "Server Responsed. Requested: %s\n", evhttp_request_get_uri(req)); <] */
-    /* evhttp_send_reply(req, HTTP_OK, "OK", buf); */
-    /* evbuffer_free(buf); */
-/* } */
-
-/* int main(int argc, char* argv[]) */
-/* { */
-
-    /* short          http_port = 8081; */
-    /* char          *http_addr = "0.0.0.0"; */
-
-    /* struct event_base * base = event_base_new(); */
-
-    /* struct evhttp * http_server = evhttp_new(base); */
-    /* if(!http_server) */
-    /* { */
-        /* return -1; */
-    /* } */
-
-    /* int ret = evhttp_bind_socket(http_server,http_addr,http_port); */
-    /* if(ret!=0) */
-    /* { */
-        /* return -1; */
-    /* } */
-
-    /* evhttp_set_gencb(http_server, generic_handler, NULL); */
-
-    /* printf("http server start OK! \n"); */
-
-    /* event_base_dispatch(base); */
-
-    /* evhttp_free(http_server); */
-
-    /* return 0; */
-/* } */
 int httpserver_bindsocket(int port, int backlog) {
     int r;
     int nfd;
@@ -141,7 +94,7 @@ void httpserver_ProcessRequest(struct evhttp_request *req) {
 }
 
 int main(int argc, char *argv[]) {
-    if(argc < 3){
+    if(argc < 4){
         printf("Usage: ./fileserver <port> <thread number> <backlog> \n");
         return 0;
     }
